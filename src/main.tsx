98e9b05fe577c1cb86b5b231d4116cd1200f0c22
Mahmoud Alaa <mahmoud.alaa.dev1@gmail.com>
2025-01-24 00:54:22 +0200

    feat: remove App component and integrate AppRouter for improved routing

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,7 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
+import ReactDOM from "react-dom/client";
+import AppRouter from "@routes/AppRouter";
 
-import App from "./App";
+//Styles
 import "bootstrap/dist/css/bootstrap.min.css";
 
-
-createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-);
+ReactDOM.createRoot(document.getElementById("root")!).render(<AppRouter />);
