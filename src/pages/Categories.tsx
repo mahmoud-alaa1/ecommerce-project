c71d49492824b6722c37b473abcdf30c24e14474
Mahmoud Alaa <mahmoud.alaa.dev1@gmail.com>
2025-01-24 07:58:14 +0200

    feat: add Category and Product components to Categories and Products pages

--- src/pages/Categories.tsx
+++ src/pages/Categories.tsx
@@ -1,3 +1,37 @@
-export default function Categories() {
-  return <div>Categories</div>;
-}
+import { Container, Row, Col } from "react-bootstrap";
+import { Category } from "@components/eCommerce";
+
+const Categories = () => {
+  return (
+    <Container>
+      <Row>
+        <Col xs={6} md={3} className="d-flex justify-content-center mb-5 mt-2">
+          <Category />
+        </Col>
+        <Col xs={6} md={3} className="d-flex justify-content-center mb-5 mt-2">
+          <Category />
+        </Col>
+        <Col xs={6} md={3} className="d-flex justify-content-center mb-5 mt-2">
+          <Category />
+        </Col>
+        <Col xs={6} md={3} className="d-flex justify-content-center mb-5 mt-2">
+          <Category />
+        </Col>
+        <Col xs={6} md={3} className="d-flex justify-content-center mb-5 mt-2">
+          <Category />
+        </Col>
+        <Col xs={6} md={3} className="d-flex justify-content-center mb-5 mt-2">
+          <Category />
+        </Col>
+        <Col xs={6} md={3} className="d-flex justify-content-center mb-5 mt-2">
+          <Category />
+        </Col>
+        <Col xs={6} md={3} className="d-flex justify-content-center mb-5 mt-2">
+          <Category />
+        </Col>
+      </Row>
+    </Container>
+  );
+};
+
+export default Categories;
--- src/pages/Products.tsx
+++ src/pages/Products.tsx
@@ -1,3 +1,27 @@
-export default function Products() {
-  return <div>Products</div>;
-}
+import { Container, Row, Col } from "react-bootstrap";
+import { Product } from "@components/eCommerce";
+const Products = () => {
+  return (
+    <Container>
+      <Row>
+        <Col xs={6} md={3} className="d-flex justify-content-center mb-5 mt-2">
+          <Product />
+        </Col>
+        <Col xs={6} md={3} className="d-flex justify-content-center mb-5 mt-2">
+          <Product />
+        </Col>
+        <Col xs={6} md={3} className="d-flex justify-content-center mb-5 mt-2">
+          <Product />
+        </Col>
+        <Col xs={6} md={3} className="d-flex justify-content-center mb-5 mt-2">
+          <Product />
+        </Col>
+        <Col xs={6} md={3} className="d-flex justify-content-center mb-5 mt-2">
+          <Product />
+        </Col>
+      </Row>
+    </Container>
+  );
+};
+
+export default Products;
