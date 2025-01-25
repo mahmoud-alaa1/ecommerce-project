f023f263bba602bb0b89e36b6235019bbab78185
Mahmoud Alaa <mahmoud.alaa.dev1@gmail.com>
2025-01-25 18:37:26 +0200

    feat: implement category fetching with typed useDispatch and useSelector hooks

--- src/pages/Categories.tsx
+++ src/pages/Categories.tsx
@@ -1,7 +1,21 @@
 import { Container, Row, Col } from "react-bootstrap";
 import { Category } from "@components/eCommerce";
 
+import { useAppDispatch, useAppSelector } from "@store/hooks";
+import { actGetCategories } from "@store/categories/categoriesSlice";
+import { useEffect } from "react";
+
 const Categories = () => {
+  const dispatch = useAppDispatch();
+
+  const { loading, error, records } = useAppSelector(
+    (state) => state.categories
+  );
+
+  useEffect(() => {
+    dispatch(actGetCategories());
+  }, [dispatch]);
+
   return (
     <Container>
       <Row>
