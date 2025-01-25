f181c479de1e628b0fc729aae3243f22896da1e5
Mahmoud Alaa <mahmoud.alaa.dev1@gmail.com>
2025-01-25 16:37:22 +0200

    refactor: rename Provider to ReduxProvider for clarity in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import ReactDOM from "react-dom/client";
 import AppRouter from "@routes/AppRouter";
 
 import "./store/categories/categoriesSlice.ts";
-import { Provider } from "react-redux";
+import { Provider as ReduxProvider } from "react-redux";
 import store from "@store/index";
 
 //Styles
@@ -10,7 +10,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "@styles/global.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <Provider store={store}>
+  <ReduxProvider store={store}>
     <AppRouter />
-  </Provider>
+  </ReduxProvider>
 );
